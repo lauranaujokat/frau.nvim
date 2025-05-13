83c3515085849b529aa4b01f99d5d632d8f994a8
Laura Naujokat <lauranaujokat@tutanota.de>
2025-05-13 17:43:09 +0200

    make a lil bit more stuff work

--- lua/frau.lua
+++ lua/frau.lua
@@ -1,29 +1,34 @@
--- Main module for the Hello World plugin
 local M = {}
 
-function M.frau()
-    local manpage = vim.fn.input("Man page: ")
-    if manpage ~= "" then
-        vim.cmd.tabnew(manpage .. ".c");
-        vim.cmd('read !man ' .. manpage);
-        vim.cmd('norm! gg')
+local function splitLines(str)
+    local lines = {}
+    for line in string.gmatch(str, "[^\r\n]+") do
+        table.insert(lines, line)
+    end
+    return lines
+end
+function M.frau(args)
+    print(args.args)
+    local manpage = args.args
+    if manpage == "" then
+        manpage = vim.fn.input("Man page: ")
+        if manpage == "" then
+            return
+        end
+    end
+    local manresult = vim.system({ "man", manpage }, { text = true }):wait()
+    if manresult.stdout ~= "" then
+        vim.cmd("e " .. manpage)
+        vim.api.nvim_buf_set_lines(0, 0, 0, false, splitLines(manresult.stdout))
+        vim.cmd("norm! gg")
+        vim.cmd("set filetype=man")
     end
 end
 
 -- Function to set up the plugin (Most package managers expect the plugin to have a setup function)
-function M.setup(opts)
-
+function M.setup()
     -- Create the user command
     vim.api.nvim_create_user_command("Frau", M.frau, { nargs = "*" })
-
-    -- Set up a key mapping
-    local keymap = '<leader>fp'
-
-    -- Create the keymap
-    vim.keymap.set('n', keymap, M.frau, {
-        desc = "Say hello from our plugin",
-        silent = true, -- Prevents the command from being echoed in the command line
-    })
 end
 
 -- Return the module
